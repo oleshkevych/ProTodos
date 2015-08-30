4cdfcf2d42e9fe9689ca25eb98b4172754376e93
Chybaka <vvvooova@gmail.com>
2015-08-30 21:15:10 +0300

    Model completed

--- model.js
+++ model.js
@@ -1,1 +1,13 @@
-var todos
+var app = app || {};
+app.todos = Backbone.model.extend({
+    defaults:{
+        title:'',
+        complete: false
+    },
+    toggle: function(){
+        "use strict";
+        this.save({
+            complete: !this.get(complete)
+        });
+    }
+});
